3ac4ba40e6d61815c880829fa266bddf9f813854
weiowyw <haxjlebatop@gmail.com>
2022-10-04 19:09:27 +0500

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let name = prompt('Enter your name:');
+
+alert('Hello, '  + name + '! How are you?');
+
+let number1 = prompt('Enter first number:');
+let number2 = prompt('Enter second number:');
+number1=parseInt(number1);
+number2=parseInt(number2);
+
+alert('Sum of your 2 numbers is ' + (number1 + number2));
+alert('Difference of your 2 numbers is ' + (number1 - number2));
+alert('Product of your 2 numbers is ' + (number1 * number2));
+alert('Quotient of your 2 numbers is ' + (number1 / number2));
+
+
+let number11 = prompt('Enter first number:');
+let number12 = prompt('Enter second number:');
+let number13 = prompt('Enter third number:');
+
+number11=parseInt(number11);
+number12=parseInt(number12);
+number13=parseInt(number13);
+alert('Arithmetic mean of these 3 numbers is ' +((number11+number12+number13)/3));
+
+
+
+
